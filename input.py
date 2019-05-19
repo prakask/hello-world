48a05a0973c6d0fd15d6372408b9b1bbe089fbc8
Prakash Karunanithi <prakash@arbornet.org>
2019-05-19 13:01:25 -0400

    rename message

---
+++ input.py
@@ -0,0 +1,19 @@
+x=20
+x-=1
+y=x
+x="prakash"
+print "we tried y=x"
+y=x
+print "y=",y
+print "x=",x
+print len(x),len(y)
+
+if x>y:
+    print "x greater"
+elif y>x:
+    print "y greater"
+else:
+    print "nothing is greater"
+
+inp=raw_input("enter the String:")
+inp1 = input("enter the Number:")
