4a2638218bbfbad38c3e24f316b262fba1ee943e
Prakash <prak_ask@yahoo.com>
2020-03-08 12:02:22 -0400

    Disable Mongodb Connection

--- pymongo_test.py
+++ pymongo_test.py
@@ -1,8 +1,8 @@
 import pymongo
 from pymongo import MongoClient
-connection = MongoClient('localhost',27017)
-db=connection.test
-movies= db.movies
+#connection = MongoClient('localhost',27017)
+#db=connection.test
+#movies= db.movies
 
-movie=movies.find_one()
-print movie['title']
+#movie=movies.find_one()
+#print movie['title']
